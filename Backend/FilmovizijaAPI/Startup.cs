32f7e0981ec23449b2361963f6e272935b010d0e
stefanmilovanovic <stefdevelops@gmail.com>
2023-02-26 22:52:10 +0100

    Inicijalni commit *** React+Typescript instalirani paketi: - npm install bootstrap@5.0 - npm i react-router-dom@5.2 - npm i --save-dev @types/react-router-dom - npm i formik@2.2.9 - npm install yup@0.32.9 - npm i react-markdown typed-react-markdown - npm install --save-exact --legacy-peer-deps  leaflet@1.7.1 react-leaflet@3.1.0 @react-leaflet/core@1.0.2 - npm i -D --legacy-peer-deps  @types/leaflet - npm i --legacy-peer-deps  @popperjs/core - npm i --legacy-peer-deps  react-bootstrap-typeahead@5.1.4 - npm i --legacy-peer-deps  -D @types/react-bootstrap-typeahead@5.1.4 *** ASP.NET Core Web API: Vraceno u stari model sa Startup klasom

---
+++ Backend/FilmovizijaAPI/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace FilmovizijaAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "FilmovizijaAPI", Version = "v1" });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
